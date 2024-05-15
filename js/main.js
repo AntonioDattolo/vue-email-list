acf68bd4a382e42c3b5ce60dc9fb0d87350c5ce6
AntonioDattolo <antonio19794.a@gmail.com>
2024-05-15 15:39:51 +0200

    revision + aesthetic changes

--- js/main.js
+++ js/main.js
@@ -14,15 +14,13 @@ const myConfig = {
 	methods: {
 		getMail() {
 			for (x = 0; x < this.howMuch; x++) {
-
 				axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((result) => {
 					let mail = result.data.response;
 					console.log("Ricevuta risposta", mail);
 					this.mailList.push(mail)
 				});
 			}
-	}
-		
+		}
 	},
 	mounted(){
 		window.vue = this
