9a3346e421bfc64acb03b7c869786c24bdd91b37
AntonioDattolo <antonio19794.a@gmail.com>
2024-05-16 10:13:09 +0200

    V-if fixed for a loop

--- js/main.js
+++ js/main.js
@@ -13,11 +13,13 @@ const myConfig = {
 	},
 	methods: {
 		getMail() {
+			this.mailList.splice(0)
 			for (x = 0; x < this.howMuch; x++) {
 				axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((result) => {
 					let mail = result.data.response;
 					console.log("Ricevuta risposta", mail);
 					this.mailList.push(mail)
+					console.log(this.mailList)
 				});
 			}
 		},
